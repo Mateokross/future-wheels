d77c81e7ad3fb0295212ea0c0772a98e98336aad
Mateokross <40373197+Mateokross@users.noreply.github.com>
2020-12-21 16:41:58 -0300

    v0

---
+++ js/init.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: 'right'
+    });
+    $('.materialboxed').materialbox();
+    $('#carousel-agrorrhh').carousel({
+        dist: -130,
+        numVisible: 3
+    });
+    $('#carousel-others').carousel({
+        dist: -30,
+        numVisible: 3
+    });
+});
---
+++ notas.md
@@ -0,0 +1,10 @@
+# Cosas a pedirle a Luke
+
+- [ ] Manual de Marca
+- [ ] Logo (en svg ideal, png segunda opción)
+- [ ] Imágenes
+- [ ] Formato
+- [ ] Textos
+- [ ] Descripción
+- [ ] Tipografias
+- [ ] Colores
