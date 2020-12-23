03335cd0e5378084f25f2622b97d392918c93e72
Mateokross <40373197+Mateokross@users.noreply.github.com>
2020-12-23 20:44:47 -0300

    v0.6

--- js/init.js
+++ js/init.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
 
-      /* ==========================================================================
-      Materialize - materializecss.com
-      ========================================================================== */
-    $('.sidenav').sidenav({
-        edge: 'right'
-    });
 
-    $('.dropdown-trigger').dropdown({
-        hover:true,
-        constrainWidth:false,
-        coverTrigger:false,
-        inDuration:400,
-        outDuration:400
-      });
-    $('.modal').modal();
+  /* ==========================================================================
+  AOS - https://michalsnik.github.io/aos/
+  ========================================================================== */
+  AOS.init({
+    offset: 300,
+    delay: 200, 
+    duration: 1800,
+    once: true
+  });
+
+
+
+  /* ==========================================================================
+  Materialize - materializecss.com
+  ========================================================================== */
+  $('.sidenav').sidenav({
+      edge: 'right'
+  });
+
+  $('.dropdown-trigger').dropdown({
+      hover:true,
+      constrainWidth:false,
+      coverTrigger:false,
+      inDuration:400,
+      outDuration:400
+    });
+  $('.modal').modal();
+
+
+
 });
